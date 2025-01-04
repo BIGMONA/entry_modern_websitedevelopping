48bfa071bf10a2eaf25fa34e8732def4956ca8b4
BIGMONA <kraffitti@gmail.com>
2025-01-04 16:18:09 +0900

    画像を表示させる

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
 import styles from "./page.module.css";
+import Image from "next/Image";
 
 export default function home() {
   return (
     <div>
-      <h1 className={styles.title}>テクノロジーの力で世界を変える！</h1>
-      <p className={styles.description}>
-        私たちは市場をリードしているグローバルテックカンパニーです。
-      </p>
+      <section className={styles.top}>
+        <h1 className={styles.title}>テクノロジーの力で世界を変える！</h1>
+        <p className={styles.description}>
+          私たちは市場をリードしているグローバルテックカンパニーです。
+        </p>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          alt=""
+          width={4000}
+          height={1200}
+        />
+      </section>
     </div>
   );
 }
